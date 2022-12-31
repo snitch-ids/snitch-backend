c2a52f7a880c957ae2da25ac629682d9a6709580
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2022-12-31 17:01:27 +0100

    don't panic api

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,66 +1,70 @@
 use crate::api::AppStateWithCounter;
+use crate::errors::ServiceError;
+use crate::errors::ServiceError::InternalServerError;
 use crate::model::user::{User, UserID};
 use crate::TokenState;
-use actix_web::{delete, get, post, web, Responder};
+use actix_web::{delete, error, get, post, web, Responder};
 use log::info;
 
 #[get("/user/{user_id}")]
 pub async fn get_user_by_id(
     user_id: web::Path<UserID>,
     state: web::Data<AppStateWithCounter>,
-) -> impl Responder {
+) -> Result<impl Responder, ServiceError> {
     let users = state.users.lock().await;
     let added_user = users
         .get_user_by_id(user_id.into_inner())
-        .expect("failed getting user");
-    format!("gotten user {added_user}")
+        .map_err(|e| InternalServerError)?;
+
+    Ok(web::Json(added_user))
 }
 
 #[get("/user")]
 pub(crate) async fn get_users(
     _user: User,
     state: web::Data<AppStateWithCounter>,
-) -> impl Responder {
+) -> Result<impl Responder, ServiceError> {
     info!("request users");
     let users = state.users.lock().await;
-    let added_user = users.get_users().expect("failed getting user");
-    format!("gotten user {added_user:?}")
+    let added_user = users.get_users().map_err(|e| InternalServerError)?;
+    Ok(web::Json(added_user))
 }
 
 #[post("/user")]
 pub(crate) async fn add_user(
     state: web::Data<AppStateWithCounter>,
     user: web::Json<User>,
-) -> impl Responder {
+    // This should be a proper actix error. Test this.
+) -> Result<impl Responder, ServiceError> {
     info!("add user");
     let mut users = state.users.lock().await;
     let added_user = users
         .add_user(user.into_inner())
-        .expect("failed adding user");
-    format!("added user {added_user}")
+        .map_err(|e| InternalServerError)?;
+    Ok(web::Json(added_user))
 }
 
 #[delete("/user/{user_id}")]
 pub(crate) async fn delete_user(
     user_id: web::Path<UserID>,
     state: web::Data<AppStateWithCounter>,
-) -> impl Responder {
+) -> Result<impl Responder, ServiceError> {
     let mut users = state.users.lock().await;
     let deleted_user = users
         .delete_user(user_id.into_inner())
-        .expect("failed deleting user");
-    format!("deleted user {deleted_user}")
+        .map_err(|e| InternalServerError)?;
+    Ok(web::Json(deleted_user))
 }
 
 #[get("/user/{user_id}/token/new")]
 pub(crate) async fn create_token(
     user_id: web::Path<UserID>,
     token_state: web::Data<TokenState>,
-) -> impl Responder {
+) -> Result<impl Responder, ServiceError> {
     info!("generate new token request");
     let mut tokens = token_state.token.lock().await;
     let token = tokens.create_token_for_user_id(&user_id.into_inner());
-    format!("{token}")
+    Ok(web::Json(token))
 }
 
 #[get("/user/{user_id}/token/")]
@@ -71,5 +75,5 @@ pub(crate) async fn get_token(
     info!("get token request");
     let tokens = token_state.token.lock().await;
     let token = tokens.get_token_of_user_id(&user_id.into_inner());
-    format!("found tokens for user {token:?}")
+    format!("{token:?}")
 }

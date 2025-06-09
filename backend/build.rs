d86ff29652849ec2aac4e05e36c7d116e984937a
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2025-06-09 22:16:42 +0200

    fix Dockerfile path in action (#12)
    * fix Dockerfile path in action
    
    * add protobuf compiler to dockerfile
    
    * add backend path to proto search path

--- backend/build.rs
+++ backend/build.rs
@@ -1,5 +1,5 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/message.proto"], &["src/"]).unwrap();
+    prost_build::compile_protos(&["src/message.proto"], &["backend/src", "src/"]).unwrap();
 }
